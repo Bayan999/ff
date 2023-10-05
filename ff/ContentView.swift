2c5aecbaf257b8830702bc887161645557a0175a
Bayan999 <128540841+Bayan999@users.noreply.github.com>
2023-10-05 11:05:41 +0300

    Initial Commit

---
+++ ff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ff
+//
+//  Created by Bayan on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ff/ffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffApp.swift
+//  ff
+//
+//  Created by Bayan on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
